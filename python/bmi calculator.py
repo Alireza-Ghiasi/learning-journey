978bd118b21f84121150d4d1f255f6dac4f5023a
Alireza-Ghiasi <alirezaghiasi69@gmail.com>
2026-07-23 03:58:30 +0330

    Add Python variables examples

--- python/bmi calculator.py
+++ python/bmi calculator.py
@@ -1,16 +1,28 @@
 a=float(input("whats yor weghit in kg ?"))
 b=float(input("whats your hight in m ?"))
-bmi = a / (b**2)
+bmi = round( a / (b**2),1)
+min_weight = 18.5 * (b ** 2)
+max_weight = 24.9 * (b ** 2)
 print ("your bmi is" , bmi)
 if bmi < 18.5:
-    print("Underweight")
+    print(f"Underweight, A healthy weight range for you is {min_weight} to {max_weight} kg")
+    need_gain = round(min_weight - a, 1)
+    print(f"You need to gain about {need_gain} kg")
 elif bmi < 25:
     print("Normal weight")
 elif bmi < 30:
-    print("Overweight")
+    print(f"Overweight, A healthy weight range for you is {min_weight} to {max_weight} kg")
+    need_lose = round(a - max_weight, 1)
+    print(f"You need to lose about {need_lose} kg")
 elif bmi < 35:
-    print("Obesity Class I")
+    print(f"Obesity Class I A healthy weight range for you is {min_weight} to {max_weight} kg")
+    need_lose = round(a - max_weight, 1)
+    print(f"You need to lose about {need_lose} kg")
 elif bmi < 40:
-    print("Obesity Class II")
+    print(f"Obesity Class II A healthy weight range for you is {min_weight} to {max_weight} kg")
+    need_lose = round(a - max_weight, 1)
+    print(f"You need to lose about {need_lose} kg")
 else:
-    print("Obesity Class III")
+    print(f"Obesity Class III A healthy weight range for you is {min_weight} to {max_weight} kg")
+    need_lose = round(a - max_weight, 1)
+    print(f"You need to lose about {need_lose} kg")
